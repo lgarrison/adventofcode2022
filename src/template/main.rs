da2181d3c5bf6d0bdcfe95452fa0ef15dedc8bc2
Lehman Garrison <lgarrison@flatironinstitute.org>
2022-12-07 20:37:10 -0500

    template tweaks

--- src/template/main.rs
+++ src/template/main.rs
@@ -4,7 +4,7 @@ fn part1(txt: &str) -> i64 {
     0
 }
 
-fn part2(txt: &str) -> i64 {
+fn part2(_txt: &str) -> i64 {
     0
 }
 
@@ -13,10 +13,11 @@ fn main() {
     let dayX = env!("CARGO_BIN_NAME");
     let root = env!("CARGO_MANIFEST_DIR");
 
-    let path = String::from(root) + "/src/" + dayX + "/input.txt";
+    //let path = String::from(root) + "/src/" + dayX + "/input.txt";
+    let path = String::from(root) + "/src/" + dayX + "/test1.txt";
     let txt = fs::read_to_string(path).unwrap();
 
     println!("This is {}", dayX);
-    println!("Part 1: {}", part1(&txt));
-    println!("Part 2: {}", part2(&txt));
+    println!("Part 1: {:?}", part1(&txt));
+    println!("Part 2: {:?}", part2(&txt));
 }
